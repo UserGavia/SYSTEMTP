d45edbf6c5a4a4207339e4eb3b0bceeae50db2a4
LALO <lalogav91@gmail.com>
2023-06-21 14:10:42 -0600

    VENTANA DE CONFIGURACION DE COSTOS

--- tecniprint/SimiSoft/ConfigCotizacionesUV.Designer.cs
+++ tecniprint/SimiSoft/ConfigCotizacionesUV.Designer.cs
@@ -31,9 +31,19 @@
             groupControl1 = new DevExpress.XtraEditors.GroupControl();
             groupControl2 = new DevExpress.XtraEditors.GroupControl();
             groupControl3 = new DevExpress.XtraEditors.GroupControl();
+            groupControl4 = new DevExpress.XtraEditors.GroupControl();
+            groupControl5 = new DevExpress.XtraEditors.GroupControl();
+            groupControl6 = new DevExpress.XtraEditors.GroupControl();
+            groupControl7 = new DevExpress.XtraEditors.GroupControl();
+            groupControl8 = new DevExpress.XtraEditors.GroupControl();
             ((System.ComponentModel.ISupportInitialize)groupControl1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)groupControl2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)groupControl3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl8).BeginInit();
             SuspendLayout();
             // 
             // groupControl1
@@ -41,33 +51,83 @@
             groupControl1.GroupStyle = DevExpress.Utils.GroupStyle.Light;
             groupControl1.Location = new System.Drawing.Point(12, 12);
             groupControl1.Name = "groupControl1";
-            groupControl1.Size = new System.Drawing.Size(252, 81);
+            groupControl1.Size = new System.Drawing.Size(252, 53);
             groupControl1.TabIndex = 0;
             groupControl1.Text = "COSTO POR TIEMPO DE IMPRESION";
             // 
             // groupControl2
             // 
             groupControl2.GroupStyle = DevExpress.Utils.GroupStyle.Light;
-            groupControl2.Location = new System.Drawing.Point(12, 99);
+            groupControl2.Location = new System.Drawing.Point(12, 71);
             groupControl2.Name = "groupControl2";
-            groupControl2.Size = new System.Drawing.Size(252, 81);
+            groupControl2.Size = new System.Drawing.Size(252, 53);
             groupControl2.TabIndex = 1;
             groupControl2.Text = "COSTO POR TIEMPO DE PLOTEADO";
             // 
             // groupControl3
             // 
             groupControl3.GroupStyle = DevExpress.Utils.GroupStyle.Light;
-            groupControl3.Location = new System.Drawing.Point(12, 186);
+            groupControl3.Location = new System.Drawing.Point(12, 130);
             groupControl3.Name = "groupControl3";
-            groupControl3.Size = new System.Drawing.Size(252, 81);
+            groupControl3.Size = new System.Drawing.Size(252, 53);
             groupControl3.TabIndex = 2;
             groupControl3.Text = "COSTO POR TIEMPO DE DEPILADO";
+            // 
+            // groupControl4
+            // 
+            groupControl4.GroupStyle = DevExpress.Utils.GroupStyle.Light;
+            groupControl4.Location = new System.Drawing.Point(270, 12);
+            groupControl4.Name = "groupControl4";
+            groupControl4.Size = new System.Drawing.Size(252, 53);
+            groupControl4.TabIndex = 3;
+            groupControl4.Text = "COSTO POR HOT-FIX";
+            // 
+            // groupControl5
+            // 
+            groupControl5.GroupStyle = DevExpress.Utils.GroupStyle.Light;
+            groupControl5.Location = new System.Drawing.Point(270, 71);
+            groupControl5.Name = "groupControl5";
+            groupControl5.Size = new System.Drawing.Size(252, 53);
+            groupControl5.TabIndex = 4;
+            groupControl5.Text = "COSTO POR TRANSPORTADOR";
+            // 
+            // groupControl6
+            // 
+            groupControl6.GroupStyle = DevExpress.Utils.GroupStyle.Light;
+            groupControl6.Location = new System.Drawing.Point(270, 130);
+            groupControl6.Name = "groupControl6";
+            groupControl6.Size = new System.Drawing.Size(252, 53);
+            groupControl6.TabIndex = 5;
+            groupControl6.Text = "COSTO POR TIEMPO DE LIMPIEZA";
+            // 
+            // groupControl7
+            // 
+            groupControl7.GroupStyle = DevExpress.Utils.GroupStyle.Light;
+            groupControl7.Location = new System.Drawing.Point(12, 189);
+            groupControl7.Name = "groupControl7";
+            groupControl7.Size = new System.Drawing.Size(510, 78);
+            groupControl7.TabIndex = 3;
+            groupControl7.Text = "COSTO POR TIPO DE LIMPIEZA";
+            // 
+            // groupControl8
+            // 
+            groupControl8.GroupStyle = DevExpress.Utils.GroupStyle.Light;
+            groupControl8.Location = new System.Drawing.Point(12, 273);
+            groupControl8.Name = "groupControl8";
+            groupControl8.Size = new System.Drawing.Size(510, 78);
+            groupControl8.TabIndex = 4;
+            groupControl8.Text = "COSTO DE TINTA";
             // 
             // ConfigCotizacionesUV
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(612, 279);
+            ClientSize = new System.Drawing.Size(538, 365);
+            Controls.Add(groupControl8);
+            Controls.Add(groupControl7);
+            Controls.Add(groupControl6);
+            Controls.Add(groupControl5);
+            Controls.Add(groupControl4);
             Controls.Add(groupControl3);
             Controls.Add(groupControl2);
             Controls.Add(groupControl1);
@@ -77,6 +137,11 @@
             ((System.ComponentModel.ISupportInitialize)groupControl1).EndInit();
             ((System.ComponentModel.ISupportInitialize)groupControl2).EndInit();
             ((System.ComponentModel.ISupportInitialize)groupControl3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)groupControl8).EndInit();
             ResumeLayout(false);
         }
 
@@ -85,5 +150,10 @@
         private DevExpress.XtraEditors.GroupControl groupControl1;
         private DevExpress.XtraEditors.GroupControl groupControl2;
         private DevExpress.XtraEditors.GroupControl groupControl3;
+        private DevExpress.XtraEditors.GroupControl groupControl4;
+        private DevExpress.XtraEditors.GroupControl groupControl5;
+        private DevExpress.XtraEditors.GroupControl groupControl6;
+        private DevExpress.XtraEditors.GroupControl groupControl7;
+        private DevExpress.XtraEditors.GroupControl groupControl8;
     }
 }
